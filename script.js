a95c5258a5a4d3fa915368d86bba2b20d43a5ae1
heishi1HUMANITY <i.wanna.make.this.main@gmail.com>
2019-11-01 23:05:15 +0900

    11-01-2019

--- script.js
+++ script.js
@@ -2,41 +2,45 @@ let update = () => {
     let day = document.querySelector('#date').value;
     let time = document.querySelector('#time').value;
     let subject = document.querySelector('#subject').value;
-    let date =  day + "-" + time;
 
     if(localStorage.getItem('todo') == null){
         let info = {
             0:{
-                date: date,
+                day: day,
+                time: time,
                 subject: subject
             }    
         };
-        console.log(JSON.stringify(info).length);
         localStorage.setItem('todo', JSON.stringify(info));
     }else{
         let strage = JSON.parse(localStorage.getItem('todo'));
-        strage[(Number(Object.keys(strage)) + 1)] = {
-            date: date,
+        strage[Number(Object.keys(strage).length)] = {
+            day: day,
+            time: time,
             subject: subject
         };    
         localStorage.setItem('todo', JSON.stringify(strage));
     }
+
+    show();
 };
 
 let show = () => {
     let todo = JSON.parse(localStorage.getItem('todo'));
     let target = document.querySelector('div');
+    target.innerHTML = "";
     if(todo != null){
+        console.log(todo);
         for(let i = 0; i < Object.keys(todo).length; i++){
-            target.innerHTML += todo[i].date + " " + todo[i].subject + "<br>";
+            target.innerHTML += todo[i].day + " " + todo[i].time + " " + todo[i].subject + "<br>";
             console.log(todo[i]);
         }
     }
-    if(todo != null) console.log(Object.keys(todo));  
 }
 
 window.addEventListener('load', show);
 
-document.querySelector('#clear').addEventListener('click', function(){
+/*document.querySelector('#clear').addEventListener('click', function(){
     localStorage.removeItem('todo');
-})
+    show();
+})*/
