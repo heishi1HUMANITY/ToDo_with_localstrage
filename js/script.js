030a8f95c028f64d1fc07566934340a5687b71a8
heishi1HUMANITY <i.wanna.make.this.main@gmail.com>
2019-11-05 13:45:22 +0900

    11-05

--- js/script.js
+++ js/script.js
@@ -1,46 +1,83 @@
-let update = () => {
-    let day = document.querySelector('#date').value;
-    let time = document.querySelector('#time').value;
-    let subject = document.querySelector('#subject').value;
+let popup = () => {
+    document.querySelector('.opacity').setAttribute('style', 'display: block;');
+    document.querySelector('#popup').setAttribute('style', 'left: 0;');
+    document.querySelector('#subject').focus();
+}
 
-    if(localStorage.getItem('todo') == null){
-        let info = {
-            0:{
-                day: day,
-                time: time,
-                subject: subject
-            }    
-        };
-        localStorage.setItem('todo', JSON.stringify(info));
-    }else{
-        let strage = JSON.parse(localStorage.getItem('todo'));
-        strage[Number(Object.keys(strage).length)] = {
-            day: day,
-            time: time,
-            subject: subject
-        };    
-        localStorage.setItem('todo', JSON.stringify(strage));
-    }
+let popupHide = () => {
+    document.querySelector('.opacity').setAttribute('style', 'display: none;');
+    document.querySelector('#popup').removeAttribute('style');
+    document.querySelector('.popupinput').value = "";
+}
 
-    show();
-};
-
-let show = () => {
+let load = () => {
     let todo = JSON.parse(localStorage.getItem('todo'));
-    let target = document.querySelector('div');
-    target.innerHTML = "";
+    document.querySelector('#contents').innerHTML = '';
     if(todo != null){
-        console.log(todo);
-        for(let i = 0; i < Object.keys(todo).length; i++){
-            target.innerHTML += todo[i].day + " " + todo[i].time + " " + todo[i].subject + "<br>";
-            console.log(todo[i]);
+        for(let i = 0; i < Number(Object.keys(todo).length); i++){
+            let insertHtml =    '<label id="' + i + '">' + 
+                                '<input type="checkbox" class="cardInput">' + 
+                                '<div class="card">' + 
+                                '<div class="top">' +
+                                '<h2>' + todo[i].subject + '</h2>' +
+                                '</div>' +
+                                '<div class="down">' + 
+                                '<h2>' + todo[i].time + '</h2>' + 
+                                '<div class="buttons">' + 
+                                '<button class="mdc-button done" onclick="done(' + i + ');">完了</button>' +
+                                '</div>' +
+                                '</div>' +
+                                '</div>' +
+                                '</label>';
+            document.querySelector('#contents').innerHTML += insertHtml;
         }
     }
 }
 
-window.addEventListener('load', show);
+let add = () => {
+    let subject = document.querySelector('#subject');
+    let time = document.querySelector('#time');
 
-/*document.querySelector('#clear').addEventListener('click', function(){
-    localStorage.removeItem('todo');
-    show();
-})*/
+    if(localStorage.getItem('todo') == null){
+        let todo = {
+            0: {
+                subject: subject.value,
+                time: time.value  
+            }
+        };
+        localStorage.setItem('todo', JSON.stringify(todo));
+    }else{
+        let todo = JSON.parse(localStorage.getItem('todo'));
+        todo[Number(Object.keys(todo).length)] = {
+            subject: subject.value,
+            time: time.value
+        };
+        localStorage.setItem('todo', JSON.stringify(todo));
+    }
+
+    load();
+    popupHide();
+}
+
+let done = (id) => {
+    let todo = JSON.parse(localStorage.getItem('todo'));
+    let length = Number(Object.keys(todo).length);
+    for(let i = 0; i < length; i++){
+        if(i == id){
+            delete todo[i];
+        }
+    }
+    for(let i = 0; i < length; i++){
+        if(i > id){
+            todo[i - 1] = todo[i];
+        }
+        if(i == length - 1){
+            delete todo[i];
+        }
+    }
+    
+    localStorage.setItem('todo', JSON.stringify(todo));
+    load();
+}
+
+window.addEventListener('load', load);
